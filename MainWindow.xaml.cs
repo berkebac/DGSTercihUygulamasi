776bf3245ca89b310c54f49c68dc1d134a36ff25
berkebac <49922154+berkebac@users.noreply.github.com>
2019-04-23 22:44:49 +0300

    Ok

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+        SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-LCGK9FQ\\SQLEXPRESS;Initial Catalog=DGSTercih;Integrated Security=True");
+
+        private void dbDeneme_Click(object sender, RoutedEventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = baglanti;
+            cmd.CommandText = "Select * From Ogrenci";
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds, "Ogrenci");
+            dgdeneme.DataContext = ds.Tables["Ogrenci"];
+
+        }
+    }
+}
